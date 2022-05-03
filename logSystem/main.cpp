4ab016733bf6457cf5c7e63f87042d5dd25e31a2
havebridge <meyl0r@yandex.com>
2022-05-03 16:29:17 +0300

    test login

--- logSystem/main.cpp
+++ logSystem/main.cpp
@@ -5,9 +5,7 @@ int main(int argc, char** argv)
 	(void)argc;
 	(void)argv;
 
-	hsbrdg::hashtable user1("login");
-
-	user1.test_hash_func();
+	hsbrdg::hashtable ht(hsbrdg::hashtable::input_user_data());
 
 	return 0;
 }
