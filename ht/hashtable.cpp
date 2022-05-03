45c4bcb2c46ea1e719b7dc5b9c9b5ed898c52897
havebridge <meyl0r@yandex.com>
2022-05-03 19:29:57 +0300

    input data

--- ht/hashtable.cpp
+++ ht/hashtable.cpp
@@ -1,29 +1,27 @@
 #include "hashtable.h"
 
-hsbrdg::hashtable::user hsbrdg::hashtable::input_user_data(void)
-{
-	std::cout << "login: ";
-	std::cin >> hsbrdg::hashtable::_user.m_login;
-
-	std::cout << "\npassword: ";
-	std::cin >> hsbrdg::hashtable::_user.m_password;
-
-	return user(hsbrdg::hashtable::_user.m_login, hsbrdg::hashtable::_user.m_password);
-}
-
-
 uint8_t hsbrdg::hashtable::hash_func(std::string key)
 {
-	int hash_val = 0;
+	uint8_t hash_val = 0;
+
 	for (int i = 0; i != key.size(); ++i)
 	{
-		hash_val += static_cast<int>(key[i]);
+		hash_val += static_cast<uint8_t>(key[i]);
 	}
 
 	return hash_val % MAX_KEY;
 }
 
-void hsbrdg::hashtable::insert(std::string login, std::string password)
+void hsbrdg::hashtable::insert(void)
 {
-	uint8_t slot = hash_func(login);	
+	hsbrdg::user data;
+	
+	std::cin >> data;
+
+	uint8_t slot = hsbrdg::hashtable::hash_func(data.m_login);
+
+	if (ht[slot] == nullptr)
+	{
+		//TODO: smart pointers
+	}
 }
