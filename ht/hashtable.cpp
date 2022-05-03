ff17a085e01d08430eeacd466101e63c34e91679
havebridge <meyl0r@yandex.com>
2022-05-03 16:27:03 +0300

    input

--- ht/hashtable.cpp
+++ ht/hashtable.cpp
@@ -1,5 +1,16 @@
 #include "hashtable.h"
 
+hsbrdg::hashtable::user hsbrdg::hashtable::input_user_data(void)
+{
+	std::cout << "login: ";
+	std::cin >> hsbrdg::hashtable::_user.m_login;
+
+	std::cout << "\npassword: ";
+	std::cin >> hsbrdg::hashtable::_user.m_password;
+
+	return user(hsbrdg::hashtable::_user.m_login, hsbrdg::hashtable::_user.m_password);
+}
+
 
 uint8_t hsbrdg::hashtable::hash_func(std::string key)
 {
@@ -10,4 +21,9 @@ uint8_t hsbrdg::hashtable::hash_func(std::string key)
 	}
 
 	return hash_val % MAX_KEY;
+}
+
+void hsbrdg::hashtable::insert(std::string login, std::string password)
+{
+	uint8_t slot = hash_func(login);	
 }
