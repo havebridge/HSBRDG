aca332f5c4e18de0d4a62b522bed460c8e9e1379
havebridge <meyl0r@yandex.com>
2022-05-05 23:06:36 +0300

    tests

--- logSystem/main.cpp
+++ logSystem/main.cpp
@@ -5,7 +5,12 @@ int main(int argc, char** argv)
 	(void)argc;
 	(void)argv;
 
-	
+	hsbrdg::hashtable _hashtable;
 
+	_hashtable.test_hash_func();
+
+	_hashtable.test_insert_func();
+
+	_hashtable.test_print_func();
 	return 0;
 }
