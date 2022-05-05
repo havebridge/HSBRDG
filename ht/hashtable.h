3a85d245fd97dced4276ca227c20aaf8162c2491
havebridge <meyl0r@yandex.com>
2022-05-05 23:05:38 +0300

    hashtable

--- ht/hashtable.h
+++ ht/hashtable.h
@@ -4,7 +4,6 @@
 #include <cstdint>
 #include <string>
 #include <iostream>
-#include <cassert>
 #include <memory>
 
 #define MAX_KEY 100
@@ -16,9 +15,6 @@ namespace hsbrdg
 		std::string m_login{};
 		std::string m_password{};
 		struct user* m_next{};
-
-	public:
-		friend std::istream& operator>> (std::istream& in, user& _user);
 
 	public:
 		user() = default;
@@ -39,14 +35,14 @@ namespace hsbrdg
 		user** ht;
 
 	public:
-		friend std::ostream& operator<< (std::ostream& out, hsbrdg::hashtable& _ht);
+		void test_hash_func(void);
+		void test_insert_func(void);
+		void test_print_func(void);
 
-		//void test_hash_func(void);
-		//void test_insert_func(void);
-		//void test_print_func(void);
-
+		void get_input(user&);
 		uint8_t hash_func(std::string key);
 		void insert(void);
+		void print_ht(void);
 
 	public:
 		hashtable()
@@ -64,44 +60,6 @@ namespace hsbrdg
 			delete ht;
 		}
 	};
-
-	std::istream& operator>> (std::istream& in, hsbrdg::user& _user)
-	{
-		std::cout << "login: ";
-		in >> _user.m_login;
-
-		std::cout << "password: ";
-		in >> _user.m_password;
-
-		return in;
-	}
-
-	std::ostream& operator<< (std::ostream& out, hsbrdg::hashtable& _ht)
-	{
-		for (int slot = 0; slot != MAX_KEY; ++slot)
-		{
-			user* tmp = _ht.ht[slot];
-
-			if (tmp == nullptr)
-			{
-				continue;
-			}
-
-			for (;;)
-			{
-				out << tmp->m_login << ' ' << tmp->m_password;
-
-				if (tmp->m_next == nullptr)
-				{
-					break;
-				}
-
-				tmp = tmp->m_next;
-			}
-		}
-
-		return out;
-	}
 };
 
 #endif
