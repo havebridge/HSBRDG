3cd82248b117d80e32f2aaaf9a0161110e0e3fe6
havebridge <meyl0r@yandex.com>
2022-05-05 23:06:05 +0300

    test of hash, insert, print

--- ht/test.cpp
+++ ht/test.cpp
@@ -1,6 +1,21 @@
 #include "hashtable.h"
 
 
-//void test_hash_func(void);
+void hsbrdg::hashtable::test_hash_func(void)
+{
+	user _user;
 
-//void test_insert_func(void);
+	hsbrdg::hashtable::get_input(_user);
+
+	std::cout << static_cast<int>(hash_func(_user.m_login)) << '\n'; // uint8_t doesn`t work with ASCII
+}
+
+void hsbrdg::hashtable::test_insert_func(void)
+{
+	insert();
+}
+
+void hsbrdg::hashtable::test_print_func(void)
+{
+	print_ht();
+}
