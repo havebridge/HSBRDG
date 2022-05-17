6b5671cce7fbedb7e6fcbf09bdcefb31a2e97286
havebridge <meyl0r@yandex.com>
2022-05-17 18:20:13 +0300

    test for good hash

--- logSystem/main.cpp
+++ logSystem/main.cpp
@@ -2,15 +2,37 @@
 
 using namespace hsbrdg;
 
+//#define TEST 1
+
 int main(int argc, char** argv)
 {
 	(void)argc;
 	(void)argv;
 
-	hashtable<std::string, std::string, 5> _hashtable;
+	hashtable<std::string, std::string, 2> _hashtable;
 
-	_hashtable.test_hash_func();
+#if TEST
+	std::vector<std::string> same;
+
+	for (int i = 0; i < 5; ++i)
+	{
+		same.push_back(std::to_string(_hashtable.test_hash_func()));
+	}
+
+	int k = 0;
+	for (int i = 1; i < 5; ++i)
+	{
+		k = 1;
+		for (int j = 0; j < i; ++j)
+		{
+			if (same.at(i) == same.at(j))
+			{
+				k++;
+			}
+		}
+	}
+	std::cout << k << std::endl;
+#endif
 	
-
 	return 0;
 }
