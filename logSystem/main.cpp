806a16b5e7d83be8577cc527d739e4f032b66611
havebridge <meyl0r@yandex.com>
2022-05-30 17:31:32 +0300

    test get function

--- logSystem/main.cpp
+++ logSystem/main.cpp
@@ -2,22 +2,39 @@
 
 using namespace hsbrdg;
 
-#define TEST_HASH 0
 #define TEST_PUT 1
+#define TEST_REMOVE 0
+#define TEST_GET 1
+#define TEST_PRINT 0
+#define TEST_HASH 0
+
 
 int main(int argc, char** argv)
 {
 	(void)argc;
 	(void)argv;
 
-	hashtable<std::string, std::string, 1> _hashtable;
+	hashtable<std::string, std::string, 100> _hashtable;
 
 #if TEST_PUT
-	_hashtable.put("q", "1");
-	_hashtable.put("q", "2");
-	_hashtable.put("q", "3");
-	_hashtable.remove("q", "1"); 
+	_hashtable.put("daaaaaaaaaaaaaaaaaaa", "af");
+	_hashtable.put("o", "meow");
+	_hashtable.put("cat", "meow");
+	_hashtable.put("g", "1007");
+	_hashtable.put("1", "inside");
+#endif
+
+#if TEST_REMOVE
+	_hashtable.remove("g", "1007");
+#endif
+
+#if TEST_PRINT
 	std::cout << _hashtable;
+#endif
+
+#if TEST_GET
+	user<std::string, std::string>* getTest = _hashtable.get("g", "1007");
+	std::cout <<  getTest->getLogin() << ' ' << getTest->getPassword() << '\n';
 #endif
 
 #if TEST_HASH
